a68950c30b5eba6b0fb890c7da688bbd52c710cf
Pedro Esteves <up201705160@fe.up.pt>
2019-10-14 16:34:43 +0100

    Added missing parts to llopen function

--- Project_1/prot_app.c
+++ Project_1/prot_app.c
@@ -8,20 +8,38 @@
 #include <stdlib.h>
 #include <signal.h>
 
-int llopen(char* gate, int flag, struct termios *oldtio)
+#define FLAG 0x7e
+#define A 0x03
+#define C_SET 0x03
+
+int flag = 1, counter = 1;
+
+/*
+ * Signal Handler for SIGALRM
+ */
+void atende(int signal);
+
+int llopen(char *gate, int flag, struct termios *oldtio)
 {
+
     if (flag != TRANSMITTER && flag != RECEIVER)
         return -1;
+
+    sighandler_t sigalrm_handler;
+
+    if(flag == TRANSMITTER){
+        sigalrm_handler = signal(SIGALRM, atende);  // instala  rotina que atende interrupcao
+    }
 
     int fd;
     struct termios newtio;
     char buf[255];
 
     if ((strcmp("/dev/ttyS0", gate) != 0) && (strcmp("/dev/ttyS1", gate) != 0)))
-    {
-        printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-        return -1;
-    }
+        {
+            printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+            return -1;
+        }
 
     /*
     Open serial port device for reading and writing and not as controlling tty
@@ -48,11 +66,13 @@ int llopen(char* gate, int flag, struct termios *oldtio)
     /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
-    if(flag == TRANSMITTER){
+    if (flag == TRANSMITTER)
+    {
         newtio.c_cc[VTIME] = 3; /* inter-character timer unused */
         newtio.c_cc[VMIN] = 0;  /* blocking read until 0 chars received */
     }
-    else{
+    else
+    {
         newtio.c_cc[VTIME] = 0; /* inter-character timer unused */
         newtio.c_cc[VMIN] = 1;  /* blocking read until 1 char received */
     }
@@ -65,6 +85,132 @@ int llopen(char* gate, int flag, struct termios *oldtio)
         return -1;
     }
 
+    int state = 0;
+    if (flag == TRANSMITTER)
+    {
+        unsigned char SET[5]; //[FLAG, A, C_SET, BCC, FLAG]
+
+        while (counter < 4 && state < 5)
+        {
+            printf("Transmission number %d\n", counter);
+            if (flag)
+            {
+                alarm(newtio.c_cc[VTIME]);
+                flag = 0;
+            }
+
+            SET[0] = FLAG;
+            SET[1] = A;
+            SET[2] = C_SET;
+            SET[3] = SET[1] ^ SET[2]; //BCC
+            SET[4] = FLAG;
+
+            if (write(fd, SET, 5) < 0)
+            {
+                perror("write");
+                exit(-1);
+            }
+
+            memset(SET, '\0', sizeof(SET));
+
+            state = 0;
+            while (state != 5) /* loop for input */
+            { 
+                if (flag == 1)
+                {
+                    printf("Receiving error at retransmission number %d\n", counter - 1);
+                    break;
+                }
+                read(fd, &SET[state], 1); /* returns after 5 chars have been input */
+                switch (state)
+                {
+                case 0:
+                    if (SET[state] == FLAG)
+                        state = 1;
+                    break;
+                case 1:
+                    if (SET[state] == A)
+                        state = 2;
+                    else if (SET[state] == FLAG)
+                        state = 1;
+                    else
+                        state = 0;
+                    break;
+                case 2:
+                    if (SET[state] == C_SET)
+                        state = 3;
+                    else if (SET[state] == FLAG)
+                        state = 1;
+                    else
+                        state = 0;
+                    break;
+                case 3:
+                    if (SET[state] == C_SET ^ A)
+                        state = 4;
+                    else
+                        state = 0;
+                    break;
+                case 4:
+                    if (SET[state] == FLAG)
+                        state = 5;
+                    else
+                        state = 0;
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+    }
+    else
+    {
+        while (state != 5)
+        {                             /* loop for input */
+            read(fd, &buf[state], 1); /* returns after 5 chars have been input */
+            switch (state)
+            {
+            case 0:
+                if (buf[state] == FLAG)
+                    state = 1;
+                break;
+            case 1:
+                if (buf[state] == A)
+                    state = 2;
+                else if (buf[state] == FLAG)
+                    state = 1;
+                else
+                    state = 0;
+                break;
+            case 2:
+                if (buf[state] == C_SET)
+                    state = 3;
+                else if (buf[state] == FLAG)
+                    state = 1;
+                else
+                    state = 0;
+                break;
+            case 3:
+                if (buf[state] == C_SET ^ A)
+                    state = 4;
+                else
+                    state = 0;
+                break;
+            case 4:
+                if (buf[state] == FLAG)
+                    state = 5;
+                else
+                    state = 0;
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    if(flag == TRANSMITTER){
+        (void) signal(SIGALRM, sigalrm_handler);
+    }
+
     return fd;
 }
 
@@ -73,17 +219,21 @@ int llwrite(int fd, char *buffer, int length)
     return write(fd, buffer, length);
 }
 
-int llread(int fd, char * buffer) {
+int llread(int fd, char *buffer)
+{
     int flag_counter = 0, char_counter = 0;
-    
-    while(flag_counter < 2) {
-        if(read(fd, &buffer[char_counter], 1) == -1) {
+
+    while (flag_counter < 2)
+    {
+        if (read(fd, &buffer[char_counter], 1) == -1)
+        {
             perror("read");
             return 1;
         }
 
-        if(buffer[char_counter] == FLAG) {
-           flag_counter++;
+        if (buffer[char_counter] == FLAG)
+        {
+            flag_counter++;
         }
 
         char_counter++;
@@ -92,13 +242,22 @@ int llread(int fd, char * buffer) {
     return char_counter;
 }
 
-int llclose(int fd, struct termios* oldtio) {
-    if (tcsetattr(fd,TCSANOW, &oldtio) == -1) {
+int llclose(int fd, struct termios *oldtio)
+{
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
         perror("tcsetattr");
         return 1;
     }
-    
+
     printf("New termios structure set\n");
-  
+
     close(fd);
+}
+
+void atende(int signal)
+{
+    printf("alarme # %d\n", counter);
+    flag = 1;
+    counter++;
 }
