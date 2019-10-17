6dd9591ce9d56eb2875ce80bb9835d13910b0b09
Pedro Esteves <36743339+pemesteves@users.noreply.github.com>
2019-10-17 12:44:14 +0100

    Changed emissor.c

--- Project_1/emissor.c
+++ Project_1/emissor.c
@@ -22,21 +22,35 @@ int main(int argc, char** argv)
 	int fd,c, res;
 	char buf[255];
 
-	if ( (argc < 2) || 
-	   ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+	if ( (argc < 2) ||
+	   ((strcmp("/dev/ttyS0", argv[1])!=0) &&
 		(strcmp("/dev/ttyS1", argv[1])!=0) &&
 		(strcmp("/dev/ttyS2", argv[1])!=0))) {
 	printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
 	exit(1);
 	}
 
-	struct termios oldtio;	
-	
-	if((fd = llopen(argv[1], TRANSMITTER, &oldtio)) < 0)
+	struct termios oldtio;
+
+	if((fd = llopen(argv[1], TRANSMITTER, &oldtio)) < 0){
 		printf("llopen error\n");
-	
-	if(llclose(fd,& oldtio))
+		return -1;
+	}
+
+	char data[5] = "Hello";
+	printf("%x\n", ((('H'^'e')^'l')^'l')^'o');
+
+  if(llwrite(fd, data, 5) < 0){
+		printf("llwrite error\n");
+		return -1;
+	}
+
+	//sleep(2);
+
+	if(llclose(fd,& oldtio)){
 		printf("llclose error\n");
+		return -1;
+	}
 
   return 0;
 }
