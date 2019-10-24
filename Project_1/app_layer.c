b9b8afce1249270748c12ef767b2e54a858536e3
pedrogalvao <pedroleitegalvao@gmail.com>
2019-10-24 12:57:27 +0100

    data_packet control_packet

--- Project_1/app_layer.c
+++ Project_1/app_layer.c
@@ -9,12 +9,18 @@
 #include <signal.h>
 #include <stdbool.h>
 
+#define START 2
+#define END 3
+
+int sequence_number = 0;
+
 int open_file(char* file_name){
     int fd = open(file_name, O_RDONLY);
     if(fd < 0){
         perror("open");
         return -1;
     }
+
 
     return 0;
 }
@@ -36,4 +42,23 @@ int close_file(int fd){
         return -1;
     }
     return 0;
+}
+
+
+int data_packet(char * string, char * packet) {
+    packet = malloc(4 + sizeof(string)/sizeof(string[0]));
+    packet[0] = 1;
+    packet[1] = sequence_number;
+    sequence_number++;
+    packet[2] = (sizeof(string)/sizeof(string[0]))/256;
+    packet[3] = (sizeof(string)/sizeof(string[0]))%256;
+    memcpy(packet + 4, string, sizeof(string)/sizeof(string[0]));
+    return 0;
+
+}
+
+int control_packet(char * packet, char * file_name, int control){
+    packet[0] = control;
+    packet[1] = get_file_size(file_name);
+    memcpy(packet + 2, file_name, sizeof(file_name)/sizeof(file_name[0]));
 }
--- Project_1/app_layer.h
+++ Project_1/app_layer.h
@@ -12,3 +12,8 @@ int read_file(int fd, unsigned char* string);
  * 
  */
 int close_file(int fd);
+
+
+int data_packet(char * string, char * packet);
+
+int control_packet(char * packet, char * file_name, int control);
