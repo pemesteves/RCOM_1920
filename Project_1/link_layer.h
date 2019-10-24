7f6f30d24299d885987d00528c0e01a6963e959c
Pedro Esteves <up201705160@fe.up.pt>
2019-10-24 17:18:07 +0100

    Miguel: Added functions to link_layer.h

--- Project_1/link_layer.h
+++ Project_1/link_layer.h
@@ -19,8 +19,22 @@ int llclose(int fd, struct termios *oldtio);
 /* Auxiliary Functions */
 
 unsigned char* create_supervision_plot(char control_field);
+int receive_supervision_plot(int fd);
+int send_supervision_plot(int fd, char control_field);
 
 unsigned char* create_information_plot(char control_field, char *data, int length);
+int receive_information_plot(int fd, unsigned char *received_plot, int *received_plot_length);
 
 int send_plot(int fd, unsigned char* plot);
 
+
+/* Calculation functions */
+
+unsigned char* retrieve_data(unsigned char *information_plot, int plot_length, int *data_length);
+unsigned char retrieve_bcc2(unsigned char *information_plot, int plot_length);
+unsigned char calculate_bcc2(unsigned char *data, int data_length);
+
+
+/* Print */
+
+void print_string(unsigned char *string, int length);
