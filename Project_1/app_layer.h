a81a4fda866249f3a64829023439537246c9b228
Pedro Esteves <36743339+pemesteves@users.noreply.github.com>
2019-10-24 13:07:58 +0100

    Update app_layer.h

--- Project_1/app_layer.h
+++ Project_1/app_layer.h
@@ -1,19 +1,3 @@
-/**
- * 
- */
-int open_file(char* file_name);
-
-/**
- * 
- */
-int read_file(int fd, unsigned char* string);
-
-/**
- * 
- */
-int close_file(int fd);
-
-
 int data_packet(char * string, char * packet);
 
 int control_packet(char * packet, char * file_name, int control);
