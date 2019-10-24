578122c97a910c0976d6ee3543edc6728ed90950
Pedro Esteves <36743339+pemesteves@users.noreply.github.com>
2019-10-24 13:07:38 +0100

    Update app_layer.c

--- Project_1/app_layer.c
+++ Project_1/app_layer.c
@@ -14,37 +14,6 @@
 
 int sequence_number = 0;
 
-int open_file(char* file_name){
-    int fd = open(file_name, O_RDONLY);
-    if(fd < 0){
-        perror("open");
-        return -1;
-    }
-
-
-    return 0;
-}
-
-
-int read_file(int fd, unsigned char* string){
-    int num_read = 0;
-    if((num_read = read(fd, string, sizeof(string)/sizeof(string[0]))) < 0){
-        perror("read");
-        return -1;
-    }
-
-    return num_read;
-}
-
-int close_file(int fd){
-    if(close(fd) < 0){
-        perror("close");
-        return -1;
-    }
-    return 0;
-}
-
-
 int data_packet(char * string, char * packet) {
     packet = malloc(4 + sizeof(string)/sizeof(string[0]));
     packet[0] = 1;
