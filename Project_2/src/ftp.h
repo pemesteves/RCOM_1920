5c31d6b3c77e2b21810815636dec5fdfd9533df0
Miguel Pinto <pt.miguel99@gmail.com>
2019-11-26 18:55:12 +0000

    Defined some server replies

--- Project_2/src/ftp.h
+++ Project_2/src/ftp.h
@@ -9,8 +9,19 @@
 #include <netdb.h>
 #include <strings.h>
 
+// Server replies
+#define FILE_OK          "150"
+#define COMMAND_OK       "200"
+#define SERVER_READY     "220"
+#define CLOSE_CONNECTION "226"
+#define USER_LOGGED      "230"
+#define DIRECTORY_OK     "257"
+#define PASSWORD_NEEDED  "331"
+
 typedef struct {
     int socket_fd;
 } FTP;
 
 int ftp_connect_server(FTP* ftp, const char* ip, int port);
+
+int ftp_server_reply(FTP* ftp, char* reply);
