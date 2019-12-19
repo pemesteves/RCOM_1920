095b053a421b8016024055174d8595d329bcd05c
Pedro Esteves <up201705160@fe.up.pt>
2019-12-19 12:42:06 +0000

    Fixed scripts

--- Project_2/scripts/tux21.sh
+++ Project_2/scripts/tux21.sh
@@ -5,3 +5,5 @@ ifconfig eth0 172.16.20.1/24
 
 #Add route
 route add -net 172.16.21.0/24 gw 172.16.20.254
+
+route add default gw 172.16.20.254
--- Project_2/scripts/tux22.sh
+++ Project_2/scripts/tux22.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 ifconfig eth0 up
-ifconfig eth0  172.16.21.1/24
+ifconfig eth0 172.16.21.1/24
 
 #Add route
 route add -net 172.16.20.0/24 gw 172.16.21.253
+
+route add default gw 172.16.21.254
+
+echo 1 > /proc/sys/net/ipv4/conf/eth0/accept_redirects
+echo 1 > /proc/sys/net/ipv4/conf/all/eth0/accept_redirect
