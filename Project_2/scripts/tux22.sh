45ca784833d777b4bc57e968ac0737ef8f2405fd
Pedro Esteves <36743339+pemesteves@users.noreply.github.com>
2019-12-20 14:14:06 +0000

    Fixed script

--- Project_2/scripts/tux22.sh
+++ Project_2/scripts/tux22.sh
@@ -9,4 +9,4 @@ route add -net 172.16.20.0/24 gw 172.16.21.253
 route add default gw 172.16.21.254
 
 echo 1 > /proc/sys/net/ipv4/conf/eth0/accept_redirects
-echo 1 > /proc/sys/net/ipv4/conf/all/eth0/accept_redirect
+echo 1 > /proc/sys/net/ipv4/conf/all/accept_redirects
